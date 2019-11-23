b37389b15af4f6aa79f711587efd75d128865cf5
vapvin <tpehdj12@daum.net>
2019-11-23 17:50:41 +0900

    New Classes Create

--- test.js
+++ test.js
@@ -1,19 +1,31 @@
 // TYPE SCRIPT
 function createBoard() {
-    var cells = [];
-    for (var row = 0; row < 4; row++) {
-        for (var col = 0; col < 3; col++) {
-            cells.push({
-                row: row,
-                col: col
-            });
-        }
+  var cells = [];
+  for (var row = 0; row < 4; row++) {
+    for (var col = 0; col < 3; col++) {
+      cells.push({
+        row: row,
+        col: col
+      });
     }
-    return cells;
+  }
+  return cells;
 }
 var board = createBoard();
 board[0].piece = {
-    move: function (from, to) {
-        return true;
-    }
+  move: function(from, to) {
+    return true;
+  }
 };
+
+class Tv {
+  constructor(remote, btn, number) {
+    this.remote = remote;
+    this.btn = btn;
+    this.number = number;
+  }
+}
+
+const gal = new Tv("내것", "니것", 97);
+
+console.log(gal);
